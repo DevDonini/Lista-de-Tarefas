145e2892c5c2c3556b08a923b990a4651bc95077
Lucas Renó <lucasreno9@gmail.com>
2022-11-07 22:50:12 -0300

    Persistido dados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { IonicStorageModule } from '@ionic/storage-angular';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
@@ -9,7 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { ToastController } from '@ionic/angular';
+import { Storage } from '@ionic/storage-angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -8,17 +9,47 @@ import { Component } from '@angular/core';
 export class HomePage {
   novaTarefa: string;
   tarefas: string[] = [];
+  tarefasBackup: string[] = [];
 
-  constructor() { }
+  constructor(private storage: Storage, private toast: ToastController) { }
 
-  adicionarTarefa() {
+  ngOnInit(){
+    this.iniciarBanco();
+  }
+
+  async iniciarBanco(){
+    await this.storage.create();
+    this.tarefas = await this.storage.get("listaTarefas") ?? [];
+  }
+
+  async adicionarTarefa() {
     if (this.novaTarefa.trim().length > 0) {
       this.tarefas.push(this.novaTarefa);
       this.novaTarefa = null;
+      await this.storage.set("listaTarefas", this.tarefas);
     }
   }
 
-  apagarTarefa(indice){
+  async apagarTarefa(indice){
+    this.tarefasBackup = [...this.tarefas];
+    this.desfazer(this.tarefas[indice]);
     this.tarefas.splice(indice,1);
+    await this.storage.set("listaTarefas", this.tarefas);
+  }
+
+  async desfazer(tarefaExcluida){
+    const t = await this.toast.create({
+      message: 'Voce excluiu ' + tarefaExcluida,
+      duration: 3000,
+      color: 'dark',
+      position: 'bottom',
+      buttons:[{
+        text: 'Desfazer',
+        handler: () => {
+          this.tarefas = [...this.tarefasBackup];
+        }
+      }]
+    });
+    t.present();
   }
 }
